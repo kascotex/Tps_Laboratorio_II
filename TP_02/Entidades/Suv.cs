539296e89f998549c82cae7e634db02a8144b4f3
unknown <leomcmlxxiv@gmail.com>
2022-04-30 09:29:29 -0300

    TP_2 primer commit

---
+++ TP_02/Entidades/Suv.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(marca, chasis, color)
+        {
+        }
+
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(".---------------------------.");
+            sb.AppendLine("|           SUV             |");
+            sb.AppendLine("|--------.------------------|");
+            sb.Append(base.Mostrar());
+
+            return sb.ToString();
+        }
+    }
+}
